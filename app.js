c27099c8115864f21118bb1f68f8c699256b43df
Matthew Fuller <matt.j.fuller@outlook.com>
2020-11-25 19:58:10 +0000

    update

--- app.js
+++ app.js
@@ -11,38 +11,30 @@ const nametop = document.querySelector(".nametop");
 
 const projects = {
     x1 : {
-        name: "Featherwold",
+        name: "Feather<span class='emph'>wold</span>",
         img: '<a target="blank" href="https://wrecket.github.io/Featherwold-new-final/"><img id="x1" class="project img-fluid" src="img/featherdeskmob.jpg" alt="featherwold"></a>',
-        description: "<p>Featherwold is an <span class='color bold'>E-commerce</span> site, built and designed following the guidelines of a customer request. The site's purpose is to sell artisan bespoke cushions and fabrics. Featherwold was built from the ground up, using bootstrap, html, CSS, Javascript, and the logo/graphics created on Photoshop.<br><span class='color bold'>Please note - This is a work in progress.</span></p>",
-        button: "<a target='blank' href='https://wrecket.github.io/Featherwold-new-final/'><button type='button' class='btn project-btn btn-warning'>Click to view</button></a>"
+        description: "<p>Featherwold is an <span class='emph'>E-commerce</span> site, built and designed following the guidelines of a customer request. The site's purpose is to sell artisan bespoke cushions and fabrics. Featherwold was built from the ground up, using bootstrap, html, CSS, Javascript, and the logo/graphics created on Photoshop.<br><span class='emph'>Please note - This is a work in progress.</span></p>",
+        button: "<a target='blank' href='https://wrecket.github.io/Featherwold-new-final/'><button type='button' class='btn project__btn btn-warning'>Click to view</button></a>"
     },
     x2 : {
-        name: "Grandma's Cookbook",
+        name: "Grandma's Cookbook <span class='emph'>(G.M.C.B)</span>",
         img: '<a target="blank" href="https://wrecket.github.io/GMCB-Final/"><img id="x2" class="project img-fluid" src="img/gmcb.JPG" alt="GMCB"></a>',
-        description: "<p>Grandma's Cookbook is an online recipe-book, built and designed following the guidelines of a customer request.The site's purpose is an easy-to-use website for an elderly client, and was built from the ground up, using html, CSS and Javascript.<br> <span class='color bold'>Please note - This is a work in progress.</span></p>",
-        button: "<a target='blank' href='https://wrecket.github.io/GMCB-Final/'><button type='button' class='btn project-btn btn-warning'>Click to view</button></a>"
+        description: "<p>Grandma's Cookbook is an online recipe-book, built and designed following the guidelines of a customer request.The site's purpose is an easy-to-use website for an elderly client, and was built from the ground up, using html, CSS and Javascript.<br> <span class='emph'>Please note - This is a work in progress.</span></p>",
+        button: "<a target='blank' href='https://wrecket.github.io/GMCB-Final/'><button type='button' class='btn project__btn btn-warning'>Click to view</button></a>"
     },
     x3 : {
-        name: "Ryan's Woodworks",
+        name: "Ryan's <span class='emph'>Woodworks</span>",
         img: '<a target="blank"href="https://wrecket.github.io/ryanswoodworksfinal/"> <img id="x3" class="project img-fluid" src="img/rwdesc.jpg" alt="Ryans Woodworks"></a>',
-        description: "<p><span class='color bold'>E-commerce</span> site made on request with guidelines. The sites purpose is to sell fully customisable handmade woodworks, and to promote the owners custom builds and personalisation business. Ryan's woodworks was built from the ground up, using bootstrap, HTML, CSS, Javascript, and the graphics editted and designed on photoshop.</p>",
-        button: "<a target='blank' href='https://wrecket.github.io/ryanswoodworksfinal/'><button type='button' class='btn project-btn btn-warning'>Click to view</button></a>"
+        description: "<p><span class='emph'>E-commerce</span> site made on request with guidelines. The sites purpose is to sell fully customisable handmade woodworks, and to promote the owners custom builds and personalisation business. Ryan's woodworks was built from the ground up, using bootstrap, HTML, CSS, Javascript, and the graphics editted and designed on photoshop.</p>",
+        button: "<a target='blank' href='https://wrecket.github.io/ryanswoodworksfinal/'><button type='button' class='btn project__btn btn-warning'>Click to view</button></a>"
     },
     x4 : {
-        name: "Grandma's Cookbook React Conversion",
+        name: "<span class='emph'>G.M.C.B</span> React Conversion",
         img: '<a href="https://wrecket.github.io/grandmascookbook.co.uk/"><img id="x4" class="project img-fluid" src="img/gmcbreact.JPG" alt="Gradient finder tool"></a>',
-        description: "<p>React app conversion of the GMCB, made with dynamicly rendered components in a one-page application. Bootstrapped with <span class='color bold'>create-react-app</span> and routed with <span class='color bold'>react-router-dom</span>. This was built with Javascript, React, HTML and CSS with Bootstrap</p>",
-        button: "<a href='https://wrecket.github.io/grandmascookbook.co.uk/'><button type='button' class='btn project-btn btn-warning'>Click to view</button></a>"
+        description: "<p>React app conversion of the GMCB, made with dynamicly rendered components in a one-page application. Bootstrapped with <span class='emph'>create-react-app</span> and routed with <span class='emph'>react-router-dom</span>. This was built with Javascript, React, HTML and CSS with Bootstrap</p>",
+        button: "<a href='https://wrecket.github.io/grandmascookbook.co.uk/'><button type='button' class='btn project__btn btn-warning'>Click to view</button></a>"
     }
 }
-
-nametop.addEventListener("onscroll", function() {
-    nametop.classList.add("animate__tada");
-    setTimeout(() => {
-        nametop.classList.remove("animate__tada")
-    }, 1000);
-})
-
 
 function projectCard(id) {
 	const p = projects[id];
@@ -73,13 +65,13 @@ document.querySelectorAll('.float-proj').forEach((item) => {
             item.style.opacity = 1;
             setTimeout(() => {
                 this.classList.remove(a);
-                this.classList.add("animate__fadeOut");
+                this.classList.add("fadeOut");
                 obj = this.id;
                 this.style.opacity = 0;
                 projectCard(obj);
                 projects.style.opacity = 1;
                 setTimeout(() => {
-                    this.classList.remove("animate__fadeOut"); 
+                    this.classList.remove("fadeOut"); 
                 }, 1000);
             }, 1000);
         });
